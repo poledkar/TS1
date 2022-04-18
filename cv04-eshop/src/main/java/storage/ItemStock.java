aab7e2a8aefd095248ecda2bd304121728e41c61
Karel Poledna <poledkar@fel.cvut.cz>
2022-04-18 15:39:43 +0200

    Fixed ItemStock null and overflow handling.

--- cv04-eshop/src/main/java/storage/ItemStock.java
+++ cv04-eshop/src/main/java/storage/ItemStock.java
@@ -2,36 +2,47 @@ package storage;
 
 import shop.*;
 
+import java.util.Objects;
+
 
 /**
  * Auxiliary class for item storage
  */
 public class ItemStock {
-    private Item refItem;
+    private final Item refItem;
     private int count;
-    
+
     ItemStock(Item refItem) {
-        this.refItem = refItem;
+        this.refItem = Objects.requireNonNull(refItem, "refItem must not be null");
         count = 0;
     }
-    
+
     @Override
     public String toString() {
-        return "STOCK OF ITEM:  "+refItem.toString()+"    PIECES IN STORAGE: "+count;
+        return "STOCK OF ITEM:  "+refItem+"    PIECES IN STORAGE: "+count;
     }
-    
-    void increaseItemCount(int x) {
-        count += x;
+
+    void increaseItemCount(int x) throws IllegalStateException {
+        addSafely(x);
     }
-    
-    void decreaseItemCount(int x) {
-        count -= x;
+
+    void decreaseItemCount(int x) throws IllegalStateException {
+        addSafely(- (long) x);
     }
-    
+
+    private void addSafely(long diff) throws IllegalStateException {
+        long result = count + diff;
+        int integer = (int) result;
+        if (integer != result) {
+            throw new IllegalStateException("count would be out of range");
+        }
+        count = integer;
+    }
+
     int getCount() {
         return count;
     }
-    
+
     Item getItem() {
         return refItem;
     }
--- cv04-eshop/src/test/java/storage/ItemStockTest.java
+++ cv04-eshop/src/test/java/storage/ItemStockTest.java
@@ -2,7 +2,6 @@ package storage;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -31,7 +30,7 @@ public class ItemStockTest {
         Assertions.assertEquals(0, new ItemStock(item).getCount());
     }
 
-    @Test @Disabled("ItemStock null handling is broken!")
+    @Test
     public void constructor_givenNullRefItem_throwsNullPointerException() {
         Assertions.assertThrows(NullPointerException.class, () -> new ItemStock(null));
     }
@@ -46,9 +45,8 @@ public class ItemStockTest {
         Assertions.assertEquals(argument, stock.getCount());
     }
 
-    @Disabled("increaseItemCount can overflow!")
     @ParameterizedTest(name = "increaseItemCount_from{0}Adds{1}_increasesTo{2}")
-    @CsvSource({"0,1,1", "1,2,3", "70,30,100", "10,-3,7", "10,-13,-3", "2000000000,1000000000,3000000000"})
+    @CsvSource({"0,1,1", "1,2,3", "70,30,100", "10,-3,7", "10,-13,-3"})
     public void increaseItemCount_twoInvocations_accumulate(int first, int second, long count) {
         ItemStock stock = new ItemStock(item);
         stock.increaseItemCount(first);
@@ -56,24 +54,43 @@ public class ItemStockTest {
         Assertions.assertEquals(count, stock.getCount());
     }
 
+    @ParameterizedTest(name = "increaseItemCount_overflowBy{0}And{1}_throwsIllegalStateException")
+    @CsvSource({"2000000000,1000000000", "1000000000,2000000000", "-2000000000,-1000000000"})
+    public void increaseItemCount_overflow_throwsIllegalStateException(int first, int second) {
+        ItemStock stock = new ItemStock(item);
+        stock.increaseItemCount(first);
+        Assertions.assertThrows(IllegalStateException.class, () -> stock.increaseItemCount(second));
+    }
+
     // parameterized test for decreaseItemCount(int)
 
-    @Disabled("Overflows for MIN_VALUE!")
     @ParameterizedTest(name = "decreaseItemCount_firstBy{0}_setsCountToMinus{0}")
-    @ValueSource(ints = {0, 1, 2, 10, 200, -333, Integer.MAX_VALUE, Integer.MIN_VALUE})
+    @ValueSource(ints = {0, 1, 2, 10, 200, -333, Integer.MAX_VALUE})
     public void decreaseItemCount_oneInvocation_setsNegatedCount(int argument) {
         ItemStock stock = new ItemStock(item);
         stock.decreaseItemCount(argument);
         Assertions.assertEquals(- (long) argument, stock.getCount());
     }
 
-    @Disabled("decreaseItemCount(int) can overflow!")
+    @Test
+    public void decreaseItemCount_minInteger_throwsIllegalStateException() {
+        Assertions.assertThrows(IllegalStateException.class, () -> new ItemStock(item).decreaseItemCount(Integer.MIN_VALUE));
+    }
+
     @ParameterizedTest(name = "decreaseItemCount_from{0}Removes{1}_result{2}")
-    @CsvSource({"0,1,-1", "10,2,8", "5,7,-2", "1234,12,1222", "-2000000000,1000000000,-3000000000"})
+    @CsvSource({"0,1,-1", "10,2,8", "5,7,-2", "1234,12,1222"})
     public void decreaseItemCount_twoInvocations_accumulate(int original, int amount, long count) {
         ItemStock stock = new ItemStock(item);
         stock.decreaseItemCount(- original);
         stock.decreaseItemCount(amount);
         Assertions.assertEquals(count, stock.getCount());
+    }
+
+    @ParameterizedTest(name = "decreaseItemCount_underflowBy{0}And{1}_throwsIllegalStateException")
+    @CsvSource({"2000000000,1000000000", "1000000000,2000000000", "-2000000000,-1000000000"})
+    public void decreaseItemCount_underflow_throwsIllegalStateException(int first, int second) {
+        ItemStock stock = new ItemStock(item);
+        stock.decreaseItemCount(first);
+        Assertions.assertThrows(IllegalStateException.class, () -> stock.decreaseItemCount(second));
     }
 }
